469b9e7d80e66cff1ad04e8c6bdb6645bda50bb2
ArunThakur9826 <at.arunthakur2001@gmail.com>
2024-10-27 03:45:47 -0700

    changes in web

--- interview/routes/web.php
+++ interview/routes/web.php
@@ -13,6 +13,10 @@ use App\Http\Controllers\Auth\AuthController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/arunchages', function () {
+    return view('welcome');
+});
+
 
 Route::get('/', function () {
     return view('welcome');
